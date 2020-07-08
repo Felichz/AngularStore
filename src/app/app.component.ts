9c649bd038892645152444ebcb36e544bed913aa
Felichz <andersson.fx@gmail.com>
2020-07-07 21:58:38 -0300

    Autologin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+
+import Swal from 'sweetalert2';
+import { takeLast, take } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { /* AppComponent clean :) */ }
+export class AppComponent {
+    private demoEmail = 'demo@demo.com';
+    private demoPwd = 'demodemo';
+
+    constructor(
+        private cookieService: CookieService,
+        private authService: AuthService
+    ) {
+        const autoLogged = this.cookieService.get('autoLogged');
+
+        if (!autoLogged) {
+            this.autoLogin();
+        }
+    }
+
+    autoLogin() {
+        this.authService
+            .getLoggedUser()
+            .pipe(take(1))
+            .subscribe(async (user) => {
+                console.log('logged? ', !!user);
+                if (!user) {
+                    try {
+                        const credentials = await this.authService.login(
+                            this.demoEmail,
+                            this.demoPwd
+                        );
+
+                        Swal.fire({
+                            icon: 'info',
+                            title: 'Logged with demo account',
+                            text: credentials.user.email,
+                        });
+
+                        const expires = new Date();
+                        expires.setDate(expires.getDate() + 1);
+                        this.cookieService.set('autoLogged', '1', expires);
+                    } catch (e) {
+                        if (e.code === 'auth/user-not-found') {
+                            console.log('user notfound');
+                            await this.authService.createUser(
+                                this.demoEmail,
+                                this.demoPwd
+                            );
+
+                            this.autoLogin();
+                        }
+                    }
+                }
+            });
+    }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 import { AuthService } from './services/auth/auth.service';
 import { CartService } from './services/cart/cart.service';
 import { ProductsService } from './services/products/products.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  providers: [
-    AuthService,
-    CartService,
-    ProductsService
-  ]
+    declarations: [],
+    imports: [],
+    providers: [AuthService, CartService, ProductsService, CookieService],
 })
-export class CoreModule { }
+export class CoreModule {}
