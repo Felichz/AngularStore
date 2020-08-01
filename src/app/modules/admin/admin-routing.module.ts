e2104588bcb1d673f34a90bb60c186403756c911
Felichz <andersson.fx@gmail.com>
2020-08-01 03:17:58 -0300

    Repaired guard bug

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -7,33 +7,45 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
+// import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+// const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/login']);
+
+import { AdminGuard } from 'src/app/guards/admin/admin.guard';
+
 const routes: Routes = [
-  {
-    path: '',
-    component: NavComponent,
-    children: [
-      {
-        path: 'product/create',
-        component: ProductFormComponent
-      },
-      {
-        path: 'product/edit/:id',
-        component: ProductEditComponent
-      },
-      {
-        path: 'product-list',
-        component: ProductListComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ]
-  }
+    {
+        path: '',
+        component: NavComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full',
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
+            },
+            {
+                path: 'product/create',
+                component: ProductFormComponent,
+            },
+            {
+                path: 'product/edit/:id',
+                component: ProductEditComponent,
+            },
+            {
+                path: 'product-list',
+                component: ProductListComponent,
+            },
+        ],
+        canActivate: [AdminGuard],
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { LayoutModule } from '@angular/cdk/layout';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ProductFormComponent } from './components/product-form/product-form.component';
@@ -11,20 +10,16 @@ import { ProductEditComponent } from './components/product-edit/product-edit.com
 import { NavComponent } from './components/nav/nav.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
-  declarations: [
-    ProductFormComponent,
-    NavComponent,
-    DashboardComponent,
-    ProductListComponent,
-    ProductEditComponent
-  ],
-  imports: [
-    AdminRoutingModule,
-    ReactiveFormsModule,
-    LayoutModule,
-    SharedModule
-  ]
+    declarations: [
+        ProductFormComponent,
+        NavComponent,
+        DashboardComponent,
+        ProductListComponent,
+        ProductEditComponent,
+    ],
+    imports: [AdminRoutingModule, ReactiveFormsModule, SharedModule],
 })
-export class AdminModule { }
+export class AdminModule {}
