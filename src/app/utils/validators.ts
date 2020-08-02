898873f980742bf8618ae14eb4d644ac4e52cb65
Felichz <andersson.fx@gmail.com>
2020-08-02 02:04:52 -0300

    File form validators

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,13 +1,38 @@
 import { AbstractControl } from '@angular/forms';
 
 export class MyValidators {
-
-    static IsValidPrice( priceField: AbstractControl ) {
+    static IsValidPrice(priceField: AbstractControl) {
         const price = priceField.value;
-        if ( price > 10000 ) {
+        if (price > 10000) {
             return { too_high: true };
         } else {
             return true;
         }
+    }
+
+    static requiredFileTypes(allowedTypes: string[]) {
+        return (imageField: AbstractControl) => {
+            if (!imageField) {
+                return true;
+            }
+            if (!allowedTypes.includes(imageField.value.type)) {
+                return { file_type: true };
+            } else {
+                return true;
+            }
+        };
+    }
+
+    static maxFileSize(maxSize: number) {
+        return (imageField: AbstractControl) => {
+            if (!imageField) {
+                return true;
+            }
+            if (imageField.value.size > maxSize) {
+                return { size_exceeded: true };
+            } else {
+                return true;
+            }
+        };
     }
 }
