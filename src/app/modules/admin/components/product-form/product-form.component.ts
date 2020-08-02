d12ed5af75d22cd46f8572287140dd9bc1af4d74
Felichz <andersson.fx@gmail.com>
2020-08-02 02:08:00 -0300

    Using image validations And disabling form when uploading and sending data

--- src/app/modules/admin/components/product-form/product-form.component.ts
+++ src/app/modules/admin/components/product-form/product-form.component.ts
@@ -11,111 +11,160 @@ import Swal from 'sweetalert2';
 import { takeLast, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
+const A_MEBIBYTE_IN_BYTES = 1049000;
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.scss']
+    selector: 'app-product-form',
+    templateUrl: './product-form.component.html',
+    styleUrls: ['./product-form.component.scss'],
 })
 export class ProductFormComponent implements OnInit {
+    uploadPercentage$: Observable<any>;
+    selectedFile: File = null;
+    productForm: FormGroup;
+    formDisabled = false;
+    invalidFormSubmit = false;
+    waiting = false;
+    waitingUpload = false;
+    uploadComplete = false;
 
-  uploadPercentage$: Observable<any>;
-  imageUrl: string;
-  productForm: FormGroup;
-  invalidFormSubmit = false;
-  waiting = false;
-  waitingUpload = false;
-  uploadComplete = false;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private productService: ProductsService,
-    private storage: AngularFireStorage
-  ) {
-    this.productForm = this.formBuilder.group({
-      image: [''],
-      title: ['', [ Validators.required ]],
-      price: ['', [ Validators.required, MyValidators.IsValidPrice ]],
-      description: ['', [ Validators.required ]]
-    });
-  }
-
-  ngOnInit() {
-  }
-
-  uploadFile(event) {
-    this.waitingUpload = true;
-    this.uploadComplete = false;
-    const file = event.target.files[0];
-
-    // Generate file name
-    const date = new Date();
-    const time = date.getTime();
-    const fileName = time + file.name;
-
-    const imagePath = `${environment.imageFolderPath}/${fileName}`;
-
-    // Start file upload task
-    const task = this.storage.upload(imagePath, file);
-
-    // Get upload progress
-    this.uploadPercentage$ = task.percentageChanges().pipe(
-      map(percentage => {
-        return percentage.toFixed(0);
-      })
-    );
-
-    // Get image HTTP URL
-    const fileRef = this.storage.ref(imagePath);
-
-    const lastSnapshot = task.snapshotChanges().pipe(
-      takeLast(1)
-    ).toPromise();
-
-    lastSnapshot.then(() => {
-      fileRef.getDownloadURL().subscribe(url => {
-        this.imageUrl = url;
-        this.imageField.setValue(url); // The form data to submit
-        this.waitingUpload = false;
-        this.uploadComplete = true;
-      });
-    });
-  }
-
-  submitForm(product) {
-
-    if (this.productForm.valid) {
-
-      this.waiting = true;
-
-      this.productService.createProduct(product).then(() => {
-
-        this.waiting = false;
-        Swal.fire({
-          title: 'New product created!',
-          icon: 'success'
-        }).then( () => {
-          this.router.navigate(['/admin/product-list']);
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private productService: ProductsService,
+        private storage: AngularFireStorage
+    ) {
+        this.productForm = this.formBuilder.group({
+            image: [
+                '',
+                [
+                    Validators.required,
+                    MyValidators.requiredFileTypes(['image/jpeg', 'image/png']),
+                    MyValidators.maxFileSize(A_MEBIBYTE_IN_BYTES),
+                ],
+            ],
+            title: ['', [Validators.required]],
+            price: ['', [Validators.required, MyValidators.IsValidPrice]],
+            description: ['', [Validators.required]],
         });
-      });
-    } else {
-      this.invalidFormSubmit = true;
     }
-  }
 
-  get priceField() {
-    return this.productForm.get('price');
-  }
+    ngOnInit() {}
 
-  get imageField() {
-    return this.productForm.get('image');
-  }
+    selectFile(event) {
+        this.selectedFile = event.target.files[0];
+        this.imageField.setValue(this.selectedFile);
+        this.imageField.markAsDirty();
+    }
 
-  get titleField() {
-    return this.productForm.get('title');
-  }
+    uploadFile() {
+        if (this.selectedFile === null) {
+            return null;
+        }
 
-  get descriptionField() {
-    return this.productForm.get('description');
-  }
+        this.waitingUpload = true;
+        this.uploadComplete = false;
+        const file = this.selectedFile;
+
+        // Generate file name
+        const date = new Date();
+        const time = date.getTime();
+        const fileName = time + file.name;
+
+        const imagePath = `${environment.imageFolderPath}/${fileName}`;
+
+        // Start file upload task
+        const task = this.storage.upload(imagePath, file);
+
+        // Get upload progress
+        this.uploadPercentage$ = task.percentageChanges().pipe(
+            map((percentage) => {
+                return percentage.toFixed(0);
+            })
+        );
+
+        // Get image HTTP URL
+        const fileRef = this.storage.ref(imagePath);
+
+        const lastSnapshot = task
+            .snapshotChanges()
+            .pipe(takeLast(1))
+            .toPromise();
+
+        return new Promise((resolve) => {
+            lastSnapshot.then(() => {
+                fileRef.getDownloadURL().subscribe((url) => {
+                    this.waitingUpload = false;
+                    this.uploadComplete = true;
+
+                    resolve(url);
+                });
+            });
+        });
+    }
+
+    submitForm() {
+        const tmpFormValue = this.productForm.value;
+
+        if (this.productForm.valid) {
+            this.waiting = true;
+            this.disableForm();
+
+            this.uploadFile().then((url) => {
+                this.productService
+                    .createProduct({ ...tmpFormValue, image: url })
+                    .then(() => {
+                        this.waiting = false;
+                        this.enableForm();
+                        Swal.fire({
+                            title: 'New product created!',
+                            icon: 'success',
+                        }).then(() => {
+                            this.router.navigate(['/admin/product-list']);
+                        });
+                    });
+            });
+        } else {
+            this.invalidFormSubmit = true;
+        }
+    }
+
+    toggleForm(shouldDisable) {
+        const toggleFields = [
+            this.titleField,
+            this.priceField,
+            this.descriptionField,
+        ];
+
+        toggleFields.forEach((field) => {
+            if (shouldDisable) {
+                field.disable();
+            } else {
+                field.enable();
+            }
+        });
+    }
+
+    disableForm() {
+        this.toggleForm(1);
+    }
+
+    enableForm() {
+        this.toggleForm(0);
+    }
+
+    get priceField() {
+        return this.productForm.get('price');
+    }
+
+    get imageField() {
+        return this.productForm.get('image');
+    }
+
+    get titleField() {
+        return this.productForm.get('title');
+    }
+
+    get descriptionField() {
+        return this.productForm.get('description');
+    }
 }
